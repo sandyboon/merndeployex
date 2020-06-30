066938d7d7061db2bfcf78df848d41d9b04d7d3c
ssingh <sandy.boon@gmail.com>
2020-06-30 19:20:15 -0400

    adding the client folder

---
+++ client/build/precache-manifest.2054265236071de57f563fd783a74ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c05fa48bd75ccb8e52a6ae5a7961c2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae778c83e6a6d4e61deb",
+    "url": "/static/css/main.1ef92e19.chunk.css"
+  },
+  {
+    "revision": "3aa593f65a3cdac8e616",
+    "url": "/static/js/2.67488170.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.67488170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae778c83e6a6d4e61deb",
+    "url": "/static/js/main.1e7924c7.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
